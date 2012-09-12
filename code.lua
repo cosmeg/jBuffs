7186df75fe9d06e6aa27796f716a093316363292
John Cramp <jbc.git@gmail.com>
2012-09-12 16:46:25 -0700

    holy shit that worked

--- code.lua
+++ code.lua
@@ -1,1 +1,10 @@
-local f = CreateFrame("frame")
+BUFFS_PER_ROW = 32;
+
+BuffFrame:ClearAllPoints()
+BuffFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -13, -13)
+
+local MSQ = LibStub("Masque")
+local masqueGroup = MSQ:Group("jBuffs", "buffs")
+for i = 1, BUFF_MAX_DISPLAY do
+  masqueGroup:AddButton(_G["BuffButton" .. i])
+end
