1008a0da572f251d5914088780ae1b9c319da95f
John Cramp <jbc.git@gmail.com>
2012-09-12 20:21:32 -0700

    notes

--- code.lua
+++ code.lua
@@ -4,7 +4,16 @@ BuffFrame:ClearAllPoints()
 BuffFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -13, -13)
 
 local MSQ = LibStub("Masque")
-local masqueGroup = MSQ:Group("jBuffs", "buffs")
+
+-- XXX new buffs aren't styled
+local masqueBuffGroup = MSQ:Group("jBuffs", "buffs")
 for i = 1, BUFF_MAX_DISPLAY do
-  masqueGroup:AddButton(_G["BuffButton" .. i])
+  masqueBuffGroup:AddButton(_G["BuffButton" .. i])
 end
+
+local masqueWeaponBuffGroup = MSQ:Group("jBuffs", "weapon buffs")
+masqueWeaponBuffGroup:AddButton(TempEnchant1)
+masqueWeaponBuffGroup:AddButton(TempEnchant2)
+-- XXX what is the stupid white border around these?
+
+-- XXX does masque do fonts?
