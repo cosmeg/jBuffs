e1dcc89c3c865fd76d594e210c67bedfb3f04250
John Cramp <jbc.git@gmail.com>
2012-09-14 01:01:07 -0700

    from drycode to reality

--- code.lua
+++ code.lua
@@ -11,15 +11,11 @@ BUFFS_PER_ROW = 32
 --- Move buff frame to the right of the screen.
 BuffFrame:ClearAllPoints()
 BuffFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -13, -13)
---XXX these points are probably wrong
---XXX does this frame even exist?
-DebuffFrame:ClearAllPoints()
-DebuffFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -13, -113)
 
 
 --- Make debuffs larger.
---XXX this may need to be set per-icon?
-DebuffFrame:SetScale(2.0)
+-- TODO this may need to be set per-icon?
+--DebuffFrame:SetScale(2.0)
 
 
 --- Style buffs as they are created.
@@ -28,20 +24,30 @@ local MSQ = LibStub("Masque")
 local masqueGroup = MSQ:Group("jBuffs", "buffs")
 
 local function StyleButton(self)
+  --print(self)
   masqueGroup:AddButton(self)
   -- TODO fonts, text positioning
 end
 
 local function UNIT_AURA(self, event, ...)
-  if ... ~= PlayerFrame.unit then return end
+  if ... ~= PlayerFrame.unit then return end  -- just our buffs
+
+  --print("UNIT_AURA")
+  --print("BUFFS_STYLED: " .. BUFFS_STYLED)
 
   while BUFFS_STYLED < BUFF_ACTUAL_DISPLAY do
     BUFFS_STYLED = BUFFS_STYLED + 1
-    StyleButton(_G["BuffFrame" .. BUFFS_STYLED])
+    print("BuffFrame" .. BUFFS_STYLED)
+    local button = _G["BuffButton" .. BUFFS_STYLED]
+    if button then
+      StyleButton(button)
+    else
+      print("XXX no button?")
+    end
   end
   while DEBUFFS_STYLED < DEBUFF_ACTUAL_DISPLAY do
     DEBUFFS_STYLED = DEBUFFS_STYLED + 1
-    StyleButton(_G["DebuffFrame" .. DEBUFFS_STYLED])
+    StyleButton(_G["DebuffButton" .. DEBUFFS_STYLED])
   end
   while ENCHANTS_STYLED < BuffFrame.numEnchants do
     ENCHANTS_STYLED = ENCHANTS_STYLED + 1
@@ -53,3 +59,6 @@ end
 local frame = CreateFrame("FRAME")
 frame:RegisterEvent("UNIT_AURA")
 frame:SetScript("OnEvent", UNIT_AURA)
+
+-- bootstrap
+UNIT_AURA(nil, "UNIT_AURA", PlayerFrame.unit)
